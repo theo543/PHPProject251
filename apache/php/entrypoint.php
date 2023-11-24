c7d2a49927bf63a8960805d82daa93721f9b63ed
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-24 10:39:25 +0200

    Move test router endpoints to a file

--- apache/php/entrypoint.php
+++ apache/php/entrypoint.php
@@ -3,20 +3,8 @@
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__));
 
 require_once("Router.php");
+require_once("test_endpoints.php");
 
 $r = new Router;
-
-$r->get("/ping_php_router_get", function() {
-    echo "Hello from the PHP router GET endpoint!<br>";
-});
-
-$r->post("/ping_php_router_post", function() {
-    echo "Hello from the PHP router POST endpoint!<br>";
-});
-
-$r->all("/ping_php_router", function() {
-    echo "Hello from the PHP router all HTTP request methods endpoint!<br>";
-    echo "Request method: " . $_SERVER["REQUEST_METHOD"];
-});
-
+register_test_endpoints($r);
 $r->run();
---
+++ apache/php/test_endpoints.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("Router.php");
+
+function register_test_endpoints(Router $r) {
+    $r->get("/ping_php_router_get", function() {
+        echo "Hello from the PHP router GET endpoint!<br>";
+    });
+
+    $r->post("/ping_php_router_post", function() {
+        echo "Hello from the PHP router POST endpoint!<br>";
+    });
+
+    $r->all("/ping_php_router", function() {
+        echo "Hello from the PHP router all HTTP request methods endpoint!<br>";
+        echo "Request method: " . $_SERVER["REQUEST_METHOD"];
+    });
+}
