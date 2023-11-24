dd77a18a81b8ce42c53051862e9eae1ef5994cd9
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-24 10:21:12 +0200

    Add more test endpoints

--- apache/php/router.php
+++ apache/php/router.php
@@ -1,7 +1,7 @@
 <?php
 
-function match_route(string $kind, string $path, callable $callback) {
-    if($_SERVER["REQUEST_METHOD"] != $kind) {
+function match_route(string | null $kind, string $path, callable $callback) {
+    if(($kind !== null) && $_SERVER["REQUEST_METHOD"] !== $kind) {
         return;
     }
     $request_path = strtok($_SERVER["REQUEST_URI"], '?');
@@ -19,7 +19,19 @@ function post(string $path, callable $callback) {
     match_route("POST", $path, $callback);
 }
 
-get("/ping_php_router", function() {
-    echo "Hello from the PHP router!<br>";
-    echo "Request URI: " . $_SERVER["REQUEST_URI"];
+function all(string $path, callable $callback) {
+    match_route(null, $path, $callback);
+}
+
+get("/ping_php_router_get", function() {
+    echo "Hello from the PHP router GET endpoint!<br>";
+});
+
+post("/ping_php_router_post", function() {
+    echo "Hello from the PHP router POST endpoint!<br>";
+});
+
+all("/ping_php_router", function() {
+    echo "Hello from the PHP router all HTTP request methods endpoint!<br>";
+    echo "Request method: " . $_SERVER["REQUEST_METHOD"];
 });
