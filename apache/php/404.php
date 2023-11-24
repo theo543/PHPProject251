4aa276296a1473f3f21159aabb33baf5f831caaa
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-24 10:43:58 +0200

    Add 404 handling

---
+++ apache/php/404.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once("Router.php");
+
+function handle_404(): void {
+    echo "404 Not Found";
+}
--- apache/php/entrypoint.php
+++ apache/php/entrypoint.php
@@ -4,7 +4,14 @@ set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__));
 
 require_once("Router.php");
 require_once("test_endpoints.php");
+require_once("404.php");
 
 $r = new Router;
+
 register_test_endpoints($r);
-$r->run();
+
+$should_404 = !$r->run();
+
+if($should_404) {
+    handle_404();
+}
