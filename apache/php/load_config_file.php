f836245e8d1a1a0fd65baade8c607c6a90ce6b06
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-12-15 06:23:31 +0200

    Fix error on null values

--- apache/php/load_config_file.php
+++ apache/php/load_config_file.php
@@ -19,7 +19,7 @@ function load_config_file(string $path, array $required_keys = array(), $default
     }
     if($required_keys !== null) {
         foreach($required_keys as $key) {
-            if(!isset($config[$key])) {
+            if(!array_key_exists($key, $config)) {
                 throw new ConfigurationException("Configuration error. Successfully loaded '$path' but the returned array does not contain key '$key'. The required keys are: " . implode(", ", $required_keys));
             }
         }
