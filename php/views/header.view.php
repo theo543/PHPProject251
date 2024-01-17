da63fe8dd21e17e8d842d2959e1c615abf5bb993
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-01-18 01:31:46 +0200

    Remove tmp files, add a favicon so Git doesn't forget the directory

--- php/views/header.view.php
+++ php/views/header.view.php
@@ -5,7 +5,8 @@
     <ELSE/>
         <title>PHP Project</title>
     </IF>
-    <!-- <link rel="stylesheet" href="/css/style.css" /> TODO style website -->
+    <link rel="icon" href="object book.svg">
+    <!-- Source: https://github.com/apancik/public-domain-icons/blob/master/dist/object%20book.svg -->
     <IF isset($recaptcha)>
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     </IF>
